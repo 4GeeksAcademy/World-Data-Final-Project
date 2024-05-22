925fd5ff775c5caaccfb242641f1f02ee297bd41
Marisol Y. Hernandez <68923545+themarisolhernandez@users.noreply.github.com>
2024-05-22 17:48:20 +0000

    Support simple drop down

--- src/app.py
+++ src/app.py
@@ -1,4 +1,18 @@
-from utils import db_connect
-engine = db_connect()
+from flask import Flask, request, render_template
+from pickle import load
 
-# your code here
+app = Flask(__name__)
+
+@app.route("/", methods = ["GET", "POST"])
+def index():
+    image_path = None
+    vehicle_type = None
+
+    if request.method == "POST":
+        vehicle_type = request.form.get("vehicle_type")
+        # By default, Flask renders static files from the static directory
+        image_path = f"/static/images/{vehicle_type}/arima_forecast_vs_actual.png"
+
+    return render_template("index.html", image_path=image_path, vehicle_type=vehicle_type)
+
+    
